943e76c96b11ca64cc34d9d7294e1ed9678a5ff2
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-10-13 12:58:23 -0400

    Samurai on the page

--- src/index.js
+++ src/index.js
@@ -10,15 +10,16 @@ canvas.height = 1000;
 const keys = [];
 
 const samurai = {
-    x: 0,
-    y: 0,
-    width: 80,
+    x: 650,
+    y: 720,
+    width: 75,
     height: 80,
     frameX: 0,
     frameY: 0,
-    speed: 9,
     moving: false
 };
+
+
 
 const samuraiSprite = new Image();
 samuraiSprite.src = "src/images/Samurai.png";
@@ -41,19 +42,60 @@ function animate() {
   ctx.clearRect(0,0, canvas.width, canvas.height);
   // ctx.drawImage(arrow, position, 0, canvas.width, canvas.height);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  position++;
-  drawSprite(samuraiSprite, 0, 0, samurai.width, samurai.height,650, 710, 3*samurai.height, 2*samurai.width);
+  // position++;
+  drawSprite(
+      samuraiSprite, 
+      samurai.width * samurai.frameX, 
+      samurai.height * samurai.frameY, 
+      samurai.width, 
+      samurai.height,
+      samurai.x, 
+      samurai.y, 
+      3*samurai.height, 
+      2*samurai.width
+    );
+    
+  punchRight();
+  punchLeft();
   requestAnimationFrame(animate);
 
 }
 
 window.addEventListener("keydown", function(e) {
-
+  keys[e.key] = true;
+  console.log(keys);
 });
 
 window.addEventListener("keyup", function(e) {
-  
+  delete keys[e.key];
 });
+
+
+// key: frameX, frame Y
+// baseright: 0, 0
+// baseleft: 6, 1
+
+// punchright1: 0, 1
+// punchright2: 1.15, 1
+// punchright3: 2.17, 1 
+
+// punchleft1: 5, 1
+// punchleft2: 4, 1
+// punchleft3: 3.05, 1 
+
+
+function punchRight(){
+  if (keys[38]) {
+      samurai.frameX = 1.15;
+      samurai.frameY = 1;
+  }
+}
+function punchLeft(){
+  if (keys[38]) {
+      samurai.frameX = 4;
+      samurai.frameY = 1;
+  }
+}
 
 document.querySelector("#game").addEventListener("click", animate);
 
