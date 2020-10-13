b28d9c3f51cbbe02ff3deaf5e51729a4a9913889
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-10-13 12:17:09 -0400

    Basic Setup Done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 import "./styles/index.scss";
-import canvas from "./scripts/canvas";
-import leftArrow from "./scripts/leftarrow";
-import rightArrow from "./scripts/rightarrow";
-import figure from "./scripts/figure";
-import leftbox from "./scripts/leftarrow";
-import rightbox from "./scripts/rightarrow";
 
-const currentStateObj = {
-  current: null,
-  currentEventListeners: [],
+
+const canvas = document.getElementById('samurai');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 1600;
+canvas.height = 1000;
+
+const keys = [];
+
+const samurai = {
+    x: 0,
+    y: 0,
+    width: 80,
+    height: 80,
+    frameX: 0,
+    frameY: 0,
+    speed: 9,
+    moving: false
 };
 
-document.querySelector("#game").addEventListener("click", startGame);
+const samuraiSprite = new Image();
+samuraiSprite.src = "src/images/Samurai.png";
+
+
+const background = new Image();
+background.src = "src/images/trees.png";
+
+const arrow = new Image();
+arrow.src = "src/images/arrow.png";
 
 
 
-function startGame() {
-  clearGame();
-  const cvs = new canvas();
-  cvs.createCanvas();
-  currentStateObj.current = "GAME";
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+let position = 0;
+
+function animate() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  // ctx.drawImage(arrow, position, 0, canvas.width, canvas.height);
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  position++;
+  drawSprite(samuraiSprite, 0, 0, samurai.width, samurai.height,650, 710, 3*samurai.height, 2*samurai.width);
+  requestAnimationFrame(animate);
+
 }
 
+window.addEventListener("keydown", function(e) {
 
-function clearGame() {
-  if(currentStateObj.current === "GAME")
-    document.body.removeChild(document.querySelector("game"));
-}
+});
+
+window.addEventListener("keyup", function(e) {
+  
+});
+
+document.querySelector("#game").addEventListener("click", animate);
 
