5353b43309deb37a0341670eac2764b06ed6b45d
NStilesMM <nicholas.mcdonnell@gmail.com>
2020-10-14 13:59:18 -0400

    score and health added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./styles/index.scss";
 
 
+// Base set up
 const canvas = document.getElementById('samurai');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 1600;
 canvas.height = 1000;
+
+
 
 const keys = [];
 
@@ -17,16 +20,17 @@ const samurai = {
     frameX: 0,
     frameY: 0,
     speed: 9,
-    health: 10,
+    health: 0,
     moving: false,
     currentAction: null,
     animationFrames: [],
 };
 
+// Setting up images
+
 
 const samuraiSprite = new Image();
 samuraiSprite.src = "src/images/Samurai.png";
-
 
 const background = new Image();
 background.src = "src/images/trees.png";
@@ -34,14 +38,23 @@ background.src = "src/images/trees.png";
 const arrow = new Image();
 arrow.src = "src/images/arrow.png";
 
+const healthSprite = new Image();
+healthSprite.src = "src/images/health.png";
 
+const gameoverImage = new Image();
+gameoverImage.src = "src/images/gameover.png";
+
+
+// DrawSprite function that allows multiple parts of an image to render
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
-let position = 0;
 
-const arrows = [];
+
+// Arrow constructing from both sides
+
+let arrows = [];
 
 function addArrow(direction) {
   const arrowSprite = new Image();
@@ -58,7 +71,7 @@ function addArrow(direction) {
     y: canvas.height - 250,
     width: 286,
     height: 58,
-    speed: 5,
+    speed: 10,
   };
   if (direction === "left") {
   
@@ -72,15 +85,38 @@ function addArrow(direction) {
   arrows.push(arrow);
 }
 
-const gameoverImage = new Image();
-gameoverImage.src = "src/images/gameover.png";
-
-
 
 
 let score = 0;
 
 
+const health = {
+    sprite: healthSprite,
+    x: canvas.width /2 - 50,
+    y: canvas.height / 2 + 150,
+    width: 600,
+    height: 110,
+    frameX: 0,
+    frameY: 0,
+  };
+
+  function currentHp() {
+      drawSprite (
+        health.sprite, 
+        0, 
+        samurai.health * 110,
+        health.width, 
+        health.height,
+        health.x, 
+        health.y,
+        health.height, 
+        0.05*health.width
+    );
+
+  }
+  // 4   3   2   1
+  // 109 220 330 440
+// Shooting arrows 
 
 let shotspeed = 2000 + Math.floor(Math.random()* 3000);
 
@@ -97,17 +133,23 @@ setInterval(() => {
   } ,shotspeed2);
 
 
+// Main animate function that runs the game
+
 function animate() {
-  let game = "ongoing"
+ 
+
+  let game = "ongoing";
+
   ctx.clearRect(0,0, canvas.width, canvas.height);
-  // setTimeout(() => {
-  // addArrow('right'); 
-  // addArrow('left');
-  // } ,5000);
   
-  
+  ctx.strokeText(score, 10, 50);
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  // position++;
+  ctx.font = "30px Roboto";
+  ctx.fillStyle = "red";
+  ctx.textAlign = "center";
+  ctx.fillText("Score;", canvas.width/2, canvas.height/2 - 100);
+  ctx.fillText(score, canvas.width/2, canvas.height/2 - 50); 
+  currentHp();
   drawSprite (
       samuraiSprite, 
       samurai.width * samurai.frameX, 
@@ -122,7 +164,6 @@ function animate() {
     for (let i = 0; i < arrows.length; i++) {
       const arrow = arrows[i];
       arrow.x += arrow.speed;
-      console.log(arrow.x);
       drawSprite(
       arrow.sprite, 
       0, 
@@ -131,27 +172,33 @@ function animate() {
       arrow.height,
       arrow.x, 
       arrow.y, 
-      .8* arrow.height, 
-      .1*arrow.width
+      0.8* arrow.height, 
+      0.1*arrow.width
 
       );
     }
     
   
-  // console.log(keys);
+
   
   punch();
   for (let i = 0; i < arrows.length; i++) {
-    if (samurai.x === arrows[i].x || samurai.x + samurai.width === arrows[i].x) {
-      samurai.health -= 1;
-      arrows.shift();
-      console.log(arrows);
-      // gameOver();
-    };
+    if (samurai.x === arrows[i].x || (samurai.x + samurai.width === arrows[i].x)) {
+      if (samurai.frameX >= 1.15) {
+        arrows = arrows.slice(i);
+        score += 1;
+      } else {
+        samurai.health += 1;
+      }
+    
+    
+
+      arrows = arrows.slice(i);
+    }
     
   }
- 
-  if (samurai.health <= 0) {
+  console.log(score);
+  if (samurai.health >= 5) {
     game = "done";
 
   }
@@ -160,6 +207,8 @@ function animate() {
   } else {
     // debugger
     ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(gameoverImage, 0, 0, canvas.width, canvas.height);
   }
 
@@ -178,7 +227,7 @@ window.addEventListener("keyup", function(e) {
 
 
 
-
+// Sprite frames for the samurai
 
 // key: frameX, frame Y
 // baseright: 0, 0
@@ -192,21 +241,15 @@ window.addEventListener("keyup", function(e) {
 // punchleft2: 4, 1
 // punchleft3: 3.05, 1 
 
-// array of x, y coordinates
 
+
+// punching animation function
 
 function punch(){
   if (keys[39] && samurai.currentAction != 39) {
       samurai.currentAction = 39;
       samurai.animationFrames = [[0,1],[0,1],[0,1],[1.15, 1],[1.15, 1],[1.15, 1],[2.17, 1],[2.17, 1],[2.17, 1]];
       
-      // if (samurai.frameX === 0 && samurai.frameY === 1) {
-      //   samurai.frameX = 1.15;
-      //   samurai.frameY = 1;
-      // } else {
-      // samurai.frameX = 0;
-      // samurai.frameY = 1;
-      // }
       
       
       
@@ -224,6 +267,7 @@ function punch(){
       samurai.frameX = 0;
       samurai.frameY = 0;
       samurai.currentAction = null;
+      
     }
   }
 }
